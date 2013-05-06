95be9eff94cfa5c1414db270baca7664ecc8606f
bbauer <bbauer@stud.hs-heilbronn.de>
2013-05-06 14:48:39 +0200

    keine Ahnung

--- beginner_tutorials/src/find_goal.cpp
+++ beginner_tutorials/src/find_goal.cpp
@@ -18,114 +18,179 @@
 class FindGoal
 {
 public:
-	FindGoal();
+  FindGoal();
 
 private:
-	void scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan);
-	void publish();
+  void scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan);
+  void publish();
+  void throughRow(const sensor_msgs::LaserScan::ConstPtr &scan);
+  float calcFieldOfAttentionX(const sensor_msgs::LaserScan::ConstPtr &scan, float &searchRange, float &angleIncrement, int &numRanges, float &robotWidth, float &x);
+  float calcFieldOfAttentionY(const sensor_msgs::LaserScan::ConstPtr &scan, float &searchRange, float &angleIncrement, int &numRanges, float &y, float &yr, float &yl);
+  void setVelocity(float &x, float &y, float &speed);
 
-	double linear;
-	double angular;
 
-	ros::NodeHandle n;
+  double linear;
+  double angular;
 
-	ros::Publisher vel_pub;
-	ros::Subscriber scan_sub;
+  ros::NodeHandle n;
 
-	ros::Timer timer;
+  ros::Publisher vel_pub;
+  ros::Subscriber scan_sub;
 
-	geometry_msgs::Twist last_published;
+  ros::Timer timer,  timer1;
+
+
+  geometry_msgs::Twist last_published;
 };
 
 FindGoal::FindGoal():
-				linear(1),
-				angular(0)
+            linear(1),
+            angular(0)
 {
-	vel_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 1);
-	scan_sub = n.subscribe<sensor_msgs::LaserScan>("scan", 10, &FindGoal::scanCallback, this);
+  vel_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 1);
+  scan_sub = n.subscribe<sensor_msgs::LaserScan>("scan", 10, &FindGoal::scanCallback, this);
 
-	timer = n.createTimer(ros::Duration(0.1), boost::bind(&FindGoal::publish, this));
+  timer = n.createTimer(ros::Duration(0.1), boost::bind(&FindGoal::publish, this));
 }
 
-void FindGoal::scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan)
+void FindGoal::scanCallback(const sensor_msgs::LaserScan::ConstPtr &scan)
 {
-	// Durch Reihe navigieren
-	int num_ranges = scan->ranges.size();
-	float angle_increment = scan->angle_increment;
+  this->throughRow(scan);
+}
 
-	float ranges[num_ranges];
+void FindGoal::throughRow(const sensor_msgs::LaserScan::ConstPtr &scan)
+{
+  // Durch Reihe navigieren
 
-	for (int i = 0; i < num_ranges; i++)
-	{
-		ranges[i] = scan->ranges[i];
-	}
+  float searchRange = 1.0;
+  int numRanges = scan->ranges.size();
+  float angleIncrement = scan->angle_increment;
 
-	float search_range = 1.0;
-	int count_r = 0, count_l = 0;
-	float y = 0, y_r = 0, y_l = 0;
+  float robotWidth = 0.5;
+  float speed = 1.0;
 
-	for (int i = 0; i < num_ranges; i++)
-	{
-		if (ranges[i] < search_range)
-		{
-			if (i < num_ranges/2)
-			{
-				y_r += (sin((i-(num_ranges/2)) * angle_increment) * ranges[i]);
-				count_r++;
-			}
-			else
-			{
-				y_l += (sin((i-(num_ranges/2)) * angle_increment) * ranges[i]);
-				count_l++;
-			}
-		}
-	}
+  float x = 0;
+  float y = 0, yr = 0, yl = 0;
 
-	if (count_r > 0)
-	{
-		y_r /= count_r;
-	}
-	if(count_l > 0)
-	{
-		y_l /= count_l;
-	}
+  x = this->calcFieldOfAttentionX(scan, searchRange, angleIncrement, numRanges, robotWidth, x);
+  y = this->calcFieldOfAttentionY(scan, searchRange, angleIncrement, numRanges, y, yr, yl);
+
+  printf("x = %f, y = %f\n",x,y);
+
+  this->setVelocity(x, y, speed);
+}
+
+float FindGoal::calcFieldOfAttentionX(const sensor_msgs::LaserScan::ConstPtr &scan, float &searchRange, float &angleIncrement, int &numRanges, float &robotWidth, float &x)
+{
+  float y = 0;
+  int count = 0;
+
+  for (int i = 0; i < numRanges; i++)
+  {
+    if (scan->ranges[i] < searchRange)
+    {
+      y = sin((i-(numRanges/2)) * angleIncrement) * scan->ranges[i];
+
+      if (y > -robotWidth/2 && y < robotWidth/2)
+      {
+        x += cos((i-(numRanges/2)) * angleIncrement) * scan->ranges[i];
+        count++;
+      }
+    }
+  }
+
+  if (count != 0)
+  {
+  x /= count;
+  }
+  else
+  {
+    x = 1;
+  }
+  return x;
+}
+
+float FindGoal::calcFieldOfAttentionY(const sensor_msgs::LaserScan::ConstPtr &scan, float &searchRange, float &angleIncrement, int &numRanges, float &y, float &yr, float &yl)
+{
+  int countr = 0, countl = 0;
+
+  for (int i = 0; i < numRanges; i++)
+  {
+    if (scan->ranges[i] < searchRange)
+    {
+      if (i < numRanges/2)
+      {
+        yr += (sin((i-(numRanges/2)) * angleIncrement) * scan->ranges[i]);
+        countr++;
+      }
+      else
+      {
+        yl += (sin((i-(numRanges/2)) * angleIncrement) * scan->ranges[i]);
+        countl++;
+      }
+    }
+  }
+
+  if (countr > 0)
+  {
+    yr /= countr;
+  }
+  if(countl > 0)
+  {
+    yl /= countl;
+  }
+
+  y = yr + yl;
+
+  return y;
+}
+
+void FindGoal::setVelocity(float &x, float &y, float &speed)
+{
+
+  if(x < 0.3)
+  {
+    linear = 0.0;
+  }
+  else
+  {
+    linear = speed;
+  }
+
+  if (y < -0.1)
+  {
+    angular = -speed;
+  }
+  else if ((y >= -0.1 && y <= 0.1) && y != 0)
+  {
+    angular = 0.0;
+  }
+  else
+  {
+    angular = speed;
+  }
 
 
-	y = y_r + y_l;
+  geometry_msgs::Twist vel;
 
-	printf("%f\n", y);
+  if (y == 0)
+  {
+  }
 
-	linear = 1;
-
-	if (y < -0.1)
-	{
-		angular = -1.0;
-	}
-	else if ((y >= -0.1 && y <= 0.1))
-	{
-		angular = 0.0;
-	}
-	else
-	{
-		angular = 1.0;
-	}
-
-	geometry_msgs::Twist vel;
-
-	vel.angular.z = angular;
-	vel.linear.x = linear;
-	last_published = vel;
+  vel.angular.z = angular;
+  vel.linear.x = linear;
+  last_published = vel;
 }
 
 void FindGoal::publish()
 {
-	vel_pub.publish(last_published);
+  vel_pub.publish(last_published);
 }
 
 int main(int argc, char** argv)
 {
-	ros::init(argc, argv, "find_goal");
-	FindGoal find_goal;
+  ros::init(argc, argv, "find_goal");
+  FindGoal find_goal;
 
-	ros::spin();
+  ros::spin();
 }
